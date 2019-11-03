568c838d323e7ffc15aaa91aebe14030bbdd5496
Kristijan Jovanovski <kristijan.j92@gmail.com>
2019-11-03 18:30:21 +0100

    Updates

---
+++ precache-manifest.b0fa5b7d7862ed4ea5adee7833dbe2eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363cda274ef5c512cf4c917a0b09d8a9",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "9f26464389de6bcf261f",
+    "url": "/react-todo-app/static/css/main.57387678.chunk.css"
+  },
+  {
+    "revision": "0d26775b0e17427a8538",
+    "url": "/react-todo-app/static/js/2.41d3fa9b.chunk.js"
+  },
+  {
+    "revision": "9f26464389de6bcf261f",
+    "url": "/react-todo-app/static/js/main.f135b761.chunk.js"
+  },
+  {
+    "revision": "f17a649cf56d1e44d3f3",
+    "url": "/react-todo-app/static/js/runtime-main.0c8055af.js"
+  }
+]);
